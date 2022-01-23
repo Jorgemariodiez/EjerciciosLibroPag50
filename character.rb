2f821246afd12192384ba6de14648c80aaf19bcb
Jorgemariodiez <jorgemariodiez@gmail.com>
2022-01-23 10:23:28 -0500

    El personaje puede atacar

--- character.rb
+++ character.rb
@@ -2,10 +2,15 @@ class Character
     def initialize
         @x = 0
         @y = 0
+        @health 0 100
     end 
     
     def move(dx, dy)
         @ += dx 
         @ += dy
-    end    
+    end 
+    
+    def attack(other)
+        other.health -= 100
+    end     
 end     
